995371b152e6200a0178dca641f9b0f4236ba1d2
Carlos <karurosualexandresouza1234@gmail.com>
2026-04-28 15:04:40 -0400

    Commit anterior

--- WACAD002 - Fundamentos de CSS/CSS3/script.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.getElementsByTagName("BUTTON");
-const cssLogo = document.getElementById("css-logo");
-
-for (var i = 0; i < buttons.length; i++) {
- buttons[i].onclick = function(e) {
- cssLogo.style.position = e.target.innerHTML;
- };
-}
