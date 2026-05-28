46d59223f357a35b1daf73380c7d9aafb691b8ec
Carlos Alexandre <karurosualexandresouza123@gmail.com>
2026-05-28 19:38:55 -0400

    EspTS 2.3

--- WACAD009 - Básico Express/ExpTS/src/index.ts
+++ WACAD009 - Básico Express/ExpTS/src/index.ts
@@ -1,17 +1,17 @@
-import express, { type Request, type Response } from "express";
-import dotenv  from "dotenv";
+import express, { type Request, type Response } from 'express';
+import dotenv from 'dotenv';
 import validateEnv from './utils/validateEnv.js';
 
 dotenv.config();
 validateEnv();
-
+const nvar = 123;
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world!");
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em: http://localhost:${PORT}`);
+    console.log(`Servidor rodando em: http://localhost:${PORT}`);
 });
