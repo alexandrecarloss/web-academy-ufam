c2efedb5f145bc6ad5a485302c06a9b74d0df12c
Carlos Alexandre <karurosualexandresouza123@gmail.com>
2026-06-06 12:29:08 -0400

    Express 2.3

--- WACAD009 - Básico Express/ExpTS/src/index.ts
+++ WACAD009 - Básico Express/ExpTS/src/index.ts
@@ -4,6 +4,7 @@ import getEnv from "./utils/getEnv.js"
 const env = getEnv()
 const PORT = env.PORT ?? 5566
 const app = express()
+const nova = 123
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello, world!")
