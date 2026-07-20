77141bf3eb7b04a6e6dff30baf49adb423217bb6
Carlos Alexandre <karurosualexandresouza123@gmail.com>
2026-07-19 22:03:46 -0400

    Cookies API

--- WACAD013 - Desenvolvimento de RESTful APIs/backend/src/index.ts
+++ WACAD013 - Desenvolvimento de RESTful APIs/backend/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import getEnv from "./utils/validateEnv.js";
 import router from "./router/index.js";
+import setCookieLang from "./middleware/setCookieLang.js";
 
 const app = express();
 const env = getEnv();
 const PORT = env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(setCookieLang)
 app.use(router);
 
 app.listen(PORT, () => {
--- WACAD013 - Desenvolvimento de RESTful APIs/backend/src/utils/validateEnv.ts
+++ WACAD013 - Desenvolvimento de RESTful APIs/backend/src/utils/validateEnv.ts
@@ -1,4 +1,4 @@
-import { cleanEnv, port, url, num } from "envalid";
+import { cleanEnv, port, url, num, str } from "envalid";
 import dotenv from "dotenv";
 
 dotenv.config({ quiet: true });
@@ -8,6 +8,7 @@ function getEnv() {
         PORT: port({default: 8999}),
         DATABASE_URL: url(),
         ROUNDS_BCRYPT: num({ default: 10 }),
+        DEFAULT_LANG: str({ default: 'pt-BR'}),
     })
 }
 
